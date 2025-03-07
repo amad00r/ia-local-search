7e4117f92e1e92d05853fe9a37930d1e56fb7998
Paracil <aracil.pol@gmail.com>
2025-03-07 19:31:36 +0100

    RedSensoresEstado -> Creada la estructura del proyecto + los principales atributos y metedos de la clase

---
+++ code/src/redsensores/RedSensoresEstado.java
@@ -0,0 +1,178 @@
+package redsensores;
+
+import java.util.ArrayList;
+import IA.Red.*;
+    
+public class RedSensoresEstado {
+
+    // TODO: Como nos sabemos si los sensores estan conectados a algo o no? conectadoA != null?
+
+    private class SensorInfo extends Sensor {
+        private int capacidadRestante;
+        private int conexionesRestantes;
+        private Object conectadoA; // Puede ser un Sensor o un Centro de Datos
+
+        public SensorInfo(int capacidad, int cx, int cy, int capacidadRestante, int conexionesRestantes, Object conectadoA) {
+            super(capacidad, cx, cy);
+            this.capacidadRestante = capacidadRestante;
+            this.conexionesRestantes = conexionesRestantes;
+            this.conectadoA = conectadoA;
+        }
+
+        public int getCapacidadRestante() {
+            return capacidadRestante;
+        }
+
+        public void updateCapacidadRestante(int capacidadConexion) {
+            if (capacidadConexion < capacidadRestante) this.capacidadRestante -= capacidadConexion;
+            else this.capacidadRestante = 0;
+        }
+
+        public int getConexionesRestantes() {
+            return conexionesRestantes;
+        }
+
+        public void recibirConexion() {
+            this.conexionesRestantes -= 1;
+        }
+
+        public Object getConectadoA() {
+            return conectadoA;
+        }
+
+        public void setConectadoA(Object conectadoA) {
+            this.conectadoA = conectadoA;
+        }
+    }
+
+    private class CentroInfo extends Centro {
+        private int capacidadRestante;
+        private int conexionesRestantes;
+
+        public CentroInfo(int cx, int cy, int capacidadRestante, int conexionesRestantes) {
+            super(cx, cy);
+            this.capacidadRestante = capacidadRestante;
+            this.conexionesRestantes = conexionesRestantes;
+        }
+
+        public int getCapacidadRestante() {
+            return capacidadRestante;
+        }
+
+        public void updateCapacidadRestante(int capacidadConexion) {
+            if (capacidadConexion < capacidadRestante) this.capacidadRestante -= capacidadConexion;
+            else this.capacidadRestante = 0;
+        }
+
+        public int getConexionesRestantes() {
+            return conexionesRestantes;
+        }
+
+        public void recibirConexion() {
+            this.conexionesRestantes -= 1;
+        }
+    }
+
+    private ArrayList<SensorInfo> sensoresInfoList = new ArrayList<>();
+    private ArrayList<CentroInfo> centrosInfoList = new ArrayList<>();
+
+    public RedSensoresEstado(int nsens, int ncent, int seed, int mode) {
+
+        //Generar sensores con la info extra
+        Sensores auxSensores = new Sensores(nsens, seed);
+        for (int i = 0; i < nsens; i++) {
+            sensoresInfoList.add(new SensorInfo((int) auxSensores.get(i).getCapacidad(), 
+                                                auxSensores.get(i).getCoordX(), 
+                                                auxSensores.get(i).getCoordY(), 
+                                                (int) auxSensores.get(i).getCapacidad() * 2, 
+                                                3, 
+                                                null));
+        }
+
+        //Generar centros de datos con la info extra
+        CentrosDatos auxCentros = new CentrosDatos(ncent, seed);
+        for (int i = 0; i < ncent; i++) {
+            centrosInfoList.add(new CentroInfo(auxSensores.get(i).getCoordX(), 
+                                                auxSensores.get(i).getCoordY(), 
+                                                150, 
+                                                3));
+        }
+
+        //Generar solucion inicial
+        if (mode == 0) solucionMala();
+        else if (mode == 1) solucionBuena();
+    }
+
+    // TODO: Implementar solucion mala + definir nombre
+    // Solucion mala -- Nombre provisional
+    private void solucionMala() {return;}
+
+    // TODO: Implementar solucion buena + definir nombre
+    //Solucion buena -- Nombre provisional
+    private void solucionBuena() {return;}
+
+    // Getters y setters de los sensores
+    public int sensorGetCoordX(int i) {
+        return sensoresInfoList.get(i).getCoordX();
+    }
+
+    public int sensorGetCoordY(int i) {
+        return sensoresInfoList.get(i).getCoordY();
+    }
+
+    public int sensorGetCapacidad(int i) {
+        return (int) sensoresInfoList.get(i).getCapacidad();
+    }
+
+    public int sensorGetCapacidadRestante(int i) {
+        return sensoresInfoList.get(i).getCapacidadRestante();
+    }
+
+    public int sensorGetConexionesRestantes(int i) {
+        return sensoresInfoList.get(i).getConexionesRestantes();
+    }
+
+    public Object sensorGetConectadoA(int i) {
+        return sensoresInfoList.get(i).getConectadoA();
+    }
+
+    public void sensorUpdateCapacidadRestante(int i, int capacidadConexion) {
+        sensoresInfoList.get(i).updateCapacidadRestante(capacidadConexion);
+    }
+
+    public void sensorRecibirConexion(int i) {
+        sensoresInfoList.get(i).recibirConexion();
+    }
+
+    public void sensorSetConectadoA(int i, Object conectadoA) {
+        sensoresInfoList.get(i).setConectadoA(conectadoA);
+    }
+
+    // Getters y setters de los centros de datos
+    public int centroGetCoordX(int i) {
+        return centrosInfoList.get(i).getCoordX();
+    }
+
+    public int centroGetCoordY(int i) {
+        return centrosInfoList.get(i).getCoordY();
+    }
+
+    public int centroGetCapacidadRestante(int i) {
+        return centrosInfoList.get(i).getCapacidadRestante();
+    }
+
+    public int centroGetConexionesRestantes(int i) {
+        return centrosInfoList.get(i).getConexionesRestantes();
+    }
+
+    public void centroUpdateCapacidadRestante(int i, int capacidadConexion) {
+        centrosInfoList.get(i).updateCapacidadRestante(capacidadConexion);
+    }
+
+    public void centroRecibirConexion(int i) {
+        centrosInfoList.get(i).recibirConexion();
+    }
+
+    // TODO: Implementar operadores + definirlos  
+    
+}
---
+++ code/src/redsensores/RedSensoresMain.java
@@ -0,0 +1,36 @@
+package redsensores;
+
+import redsensores.RedSensoresEstado;
+import java.util.Scanner;
+
+public class RedSensoresMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el número de sensores: ");
+        int numSensores = scanner.nextInt();
+
+        System.out.print("Ingrese el número de centros de datos: ");
+        int numCentros = scanner.nextInt();
+
+        System.out.print("Ingrese la semilla para el generador de números aleatorios: ");
+        int semilla = scanner.nextInt();
+
+        System.out.print("Ingrese el modo de generación: ");
+        int mode = scanner.nextInt();
+
+        scanner.close();
+
+        RedSensoresEstado redSensores = new RedSensoresEstado(numSensores, numCentros, semilla, mode);
+        
+        System.out.println("Coordenadas de los sensores:");
+        for (int i = 0; i < numSensores; i++) {
+            System.out.println("Sensor " + i + ": (" + redSensores.sensorGetCoordX(i) + ", " + redSensores.sensorGetCoordY(i) + ") -> Capacidad: " + redSensores.sensorGetCapacidad(i) + " --> Conexiones restantes: " + redSensores.sensorGetConexionesRestantes(i) + " --> Capacidad restante: " + redSensores.sensorGetCapacidadRestante(i));     
+        }
+        
+        System.out.println("Coordenadas de los centros de datos:");
+        for (int i = 0; i < numCentros; i++) {
+            System.out.println("Centro de datos " + i + ": (" + redSensores.centroGetCoordX(i) + ", " + redSensores.centroGetCoordY(i) + ")" + " --> Capacidad restante: " + redSensores.centroGetCapacidadRestante(i) + " --> Conexiones restantes: " + redSensores.centroGetConexionesRestantes(i));
+        }
+    }
+}
