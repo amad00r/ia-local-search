1e68024c54b6cf83b98f77c1d0f8ef3f7d917dfd
Paracil <aracil.pol@gmail.com>
2025-03-16 16:46:23 +0100

    goal test

---
+++ src/RedSensoresGoalTest.java
@@ -0,0 +1,11 @@
+package redsensores;
+
+import aima.search.framework.GoalTest;
+
+public class RedSensoresGoalTest implements GoalTest {
+
+    @Override
+    public boolean isGoalState(Object state) {
+        return false;
+    }
+}
